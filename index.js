097826dfce46d17321abb225a971367f8b65dc5c
Emily Deng <emrdeng21@gmail.com>
2023-08-23 05:18:21 -0400

    Update

--- index.js
+++ index.js
@@ -12,11 +12,27 @@ require("./models/passport");
 
 let app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: "GET, POST, PUT, DELETE",
-  credentials: true,
-}));
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   methods: "GET, POST, PUT, DELETE",
+//   credentials: true,
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -47,6 +63,16 @@ app.use((req,res,next)=>{
 
 app.use("/auth", authHandler);
 app.use("/", routeHandler);
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT,function(req,res){
     console.log(`Server is running on Port ${process.env.PORT}.`)
